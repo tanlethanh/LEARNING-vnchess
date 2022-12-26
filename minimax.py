3b773d1f48a39adac5e61a52fd4318a0f276031d
Tan Le <tanlethanh8266@gmail.com>
2022-12-26 15:32:03 +0700

    hotfix: toggle player number in next node

--- minimax.py
+++ minimax.py
@@ -3,7 +3,7 @@
 import builtins
 import sys
 
-import numpy
+import numpy as np
 
 from action import Action
 from game_manager import get_active_position, get_traps, get_actions_of_chessman, copy_board, update_board
@@ -14,7 +14,7 @@ MAX, MIN = 1000, -1000
 
 
 def evaluation(node):
-    score = numpy.sum(node.board)
+    score = np.sum(node.board)
     # if score == 16:
     #     print("-------------------------------------- WIN --------------------------------------")
     #     sys.exit()
@@ -73,6 +73,7 @@ def get_all_actions(_prev_board, _board, _player_num):
                     actions = get_actions_of_chessman(_board, (i, j))
                     all_actions += [((i, j), blind_move((i, j), action)) for action in actions]
 
+    np.random.shuffle(all_actions)
     return all_actions
 
 
@@ -83,22 +84,32 @@ def take_action(_prev_board, _board, _player_num, _action):
     start, end = _action
     updated_board = update_board(_prev_board, copy_board(_board), start, end, _player_num)
 
-    return Node(_board, updated_board, _player_num)
+    # print(f"In MINIMAX: _player_num: {_player_num}")
+    return Node(_board, updated_board, -_player_num)
 
 
 def minimax(node: Node, is_max_player, alpha, beta, depth, max_depth=3):
     if depth == max_depth:
         return node.get_value()
 
+    # if is_max_player:
+    #     print("MAX phase")
+    # else:
+    #     print("MIN phase")
+
     best, choose = (MIN, builtins.max) if is_max_player else (MAX, builtins.min)
 
     list_action = get_all_actions(node.prev_board, node.board, node.player_num)
+
+    if len(list_action) == 0:
+        print("End game here")
 
     for action in list_action:
         child = take_action(node.prev_board, node.board, node.player_num, action)
         node.append_child(child, action)
         value = minimax(child, not is_max_player, alpha, beta, depth + 1, max_depth)
         best = choose(best, value)
+
         node.set_value(best)
 
         if is_max_player:
@@ -107,16 +118,55 @@ def minimax(node: Node, is_max_player, alpha, beta, depth, max_depth=3):
             beta = choose(best, beta)
 
         if alpha >= beta:
+            # print(f"Pruning: alpha = {alpha}, beta: {beta}")
             break
 
+    node.set_value(best)
     return best
 
 
 def move(_prev_board, _board, _player, _remain_time_x, _remain_time_o):
     cur_node = Node(_prev_board, _board, _player)
 
-    max_value = minimax(node=cur_node, is_max_player=True, alpha=MIN, beta=MAX, depth=0, max_depth=3)
+    is_max = False if _player == -1 else True
+    max_value = minimax(node=cur_node, is_max_player=is_max, alpha=MIN, beta=MAX, depth=0, max_depth=3)
 
+    # print(max_value)
+    # print([child.get_value() for child in cur_node.children])
+    for child in cur_node.children:
+        if max_value == child.get_value():
+            start, end = child.action
+            print(f"\t MINIMAX move: {start} -> {end}")
+            return child.action
+
+    return None
+
+
+def move_depth_3(_prev_board, _board, _player, _remain_time_x, _remain_time_o):
+    cur_node = Node(_prev_board, _board, _player)
+
+    is_max = False if _player == -1 else True
+    max_value = minimax(node=cur_node, is_max_player=is_max, alpha=MIN, beta=MAX, depth=0, max_depth=3)
+
+    # print(max_value)
+    # print([child.get_value() for child in cur_node.children])
+    for child in cur_node.children:
+        if max_value == child.get_value():
+            start, end = child.action
+            print(f"\t MINIMAX move: {start} -> {end}")
+            return child.action
+
+    return None
+
+
+def move_depth_4(_prev_board, _board, _player, _remain_time_x, _remain_time_o):
+    cur_node = Node(_prev_board, _board, _player)
+
+    is_max = False if _player == -1 else True
+    max_value = minimax(node=cur_node, is_max_player=is_max, alpha=MIN, beta=MAX, depth=0, max_depth=4)
+
+    # print(max_value)
+    # print([child.get_value() for child in cur_node.children])
     for child in cur_node.children:
         if max_value == child.get_value():
             start, end = child.action
